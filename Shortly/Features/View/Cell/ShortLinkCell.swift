c5144e3ab1fc91d59aa080a53226911acdc04072
Nevin Ozkan <ozkannevin73@gmail.com>
2024-12-17 11:23:45 +0300

    Created onBoard Screen

---
+++ Shortly/Features/View/Cell/ShortLinkCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShortLinkCell.swift
+//  Shortly
+//
+//  Created by Nevin Özkan on 16.12.2024.
+//
+
+import UIKit
+
+class ShortLinkCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Shortly/Features/View/OnBoardScreen/OnboardingPage1VC.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingPage1VC.swift
+//  Shortly
+//
+//  Created by Nevin Özkan on 16.12.2024.
+//
+
+import UIKit
+
+class OnboardingPage1VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
