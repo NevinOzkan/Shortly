77e3f5c64289d29c4e48a29b43b9d98f0f8251a5
Nevin Ozkan <ozkannevin73@gmail.com>
2024-12-19 19:54:32 +0300

    Swift Data Eklendi

--- Shortly/Base/Network/ApıService.swift
+++ Shortly/Base/Network/ApıService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Alamofire
+
+protocol ApıServiceProtocol {
+    func fetchLinks(completion: @escaping (Result<[Link], Error>) -> Void)
+}
+
+enum ApiServiceError: Error {
+    case networkError(internal: Error)
+    case serializationError(internal: Error)
+}
+
+class ApiService: ApıServiceProtocol {
+    
+    private let apiKey = "8cf5564564384ee7adf398e553335ccb"
+    private let baseUrl = "https://api.rebrandly.com/v1/links"
+    
+    func fetchLinks(completion: @escaping (Result<[Link], any Error>) -> Void) {
+        let url = "\(baseUrl)?apikey=\(apiKey)&action=listLinks"
+        
+        AF.request(url).responseData { <#AFDataResponse<Data>#> in
+            <#code#>
+        }
+    }
+    
+    
+}
